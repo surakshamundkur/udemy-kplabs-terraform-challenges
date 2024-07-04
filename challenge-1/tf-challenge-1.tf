78b0c031132a02d5e4008b4b90a5b1b475a71e84
surakshamundkur <154885639+surakshamundkur@users.noreply.github.com>
2024-07-05 01:20:39 +0530

    Update tf-challenge-1.tf

--- challenge-1/tf-challenge-1.tf
+++ challenge-1/tf-challenge-1.tf
@@ -1,14 +1,14 @@
 provider "aws" {
   version = "~> 2.54"
   region  = "us-east-1"
-  access_key = "AKIAIOSFODNN7EXAMPLE"
+  access_key = "AKIAIOSFODNN7EXAMPLE" #add your access key
   secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 }
 
 provider "digitalocean" {}
 
 terraform {
-    required_version = "0.12.31"
+    required_version = "0.12.31"  # check for current version
 }
 
 
